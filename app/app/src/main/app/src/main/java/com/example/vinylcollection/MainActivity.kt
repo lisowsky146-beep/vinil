21c64da34b8b0b92c7dc94542088dbbacc4f0912
lisowsky146-beep <lisowsky146@gmail.com>
2026-03-14 00:50:18 +0500

    Create MainActivity.kt

---
+++ app/app/src/main/app/src/main/java/com/example/vinylcollection/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.vinylcollection
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "Vinyl Collection"
+        text.textSize = 24f
+
+        setContentView(text)
+    }
+}
